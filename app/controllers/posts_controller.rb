643021a5c0afab956cd8ae2581d754745b7e9095
github username <github email address>
2019-05-13 15:04:38 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,20 @@
 class PostsController < ApplicationController
 
+  # app/controllers/posts_controller.rb
+
   def index
-    @posts = Post.all
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
   end
 
   def show
     @post = Post.find(params[:id])
   end
+
+
 
   def new
     @post = Post.new
